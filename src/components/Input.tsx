08b030f391dc15d0064f37321cc3dc5e0307af5a
Webprojon <tokhirjonkhasanov@gmail.com>
2025-02-03 11:17:25 +0100

    error fixed

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,15 +1,25 @@
-export default function Input({ type, name = "", text = "", size = "" }) {
+interface InputProps {
+	type: string;
+	name?: string;
+	text?: string;
+	size?: string;
+}
+
+export default function Input({
+	type,
+	name = "",
+	text = "",
+	size = "",
+}: InputProps) {
 	return (
-		<>
-			<input
-				type={type}
-				name={name}
-				placeholder={text}
-				autoComplete="off"
-				className={`outline-green-500 tracking-wider text-gray-500 placeholder:text-gray-500 px-3 py-2 rounded-sm
-				dark:outline-none dark:bg-transparent dark:text-gray-300 dark:placeholder:text-gray-300 bg-transparent border border-gray-400 dark:border-gray-700
-			 ${size === "sm" ? "w-[8rem]" : ""} `}
-			/>
-		</>
+		<input
+			type={type}
+			name={name}
+			placeholder={text}
+			autoComplete="off"
+			className={`outline-green-500 tracking-wider text-gray-500 placeholder:text-gray-500 px-3 py-2 rounded-sm
+        dark:outline-none dark:bg-transparent dark:text-gray-300 dark:placeholder:text-gray-300 bg-transparent border border-gray-400 dark:border-gray-700
+        ${size === "sm" ? "w-[8rem]" : ""}`}
+		/>
 	);
 }
