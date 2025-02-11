7456bfc2413df13e4e9f77d3fe4e46f6c5116937
Webprojon <tokhirjonkhasanov@gmail.com>
2025-02-11 21:11:19 +0100

    fixed

--- src/app/menu/[id]/page.tsx
+++ src/app/menu/[id]/page.tsx
@@ -15,8 +15,6 @@ export default async function Product({ params }: { params: { id: string } }) {
 		return <div>Product not found</div>;
 	}
 
-	const numericIndex = parseInt(product.id as string, 10);
-
 	return (
 		<div className="md:flex gap-x-10 items-end justify-between max-w-[1250px] mx-auto md:p-7 rounded-md bg-slate-100 dark:bg-black/40 backdrop-blur-sm">
 			<Link href={"/menu"} className="absolute top-6 right-5">
@@ -42,7 +40,7 @@ export default async function Product({ params }: { params: { id: string } }) {
 			</div>
 
 			<div className="md:w-[45%]">
-				<AddToCart product={product} index={numericIndex} />
+				<AddToCart product={product} />
 			</div>
 		</div>
 	);
