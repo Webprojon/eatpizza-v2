b5b32dead7baffd47c1604d9d3aa59aba0179948
Webprojon <tokhirjonkhasanov@gmail.com>
2025-02-12 11:07:51 +0100

    Mobile basket is added

--- prisma/migrations/20250212100505_init/migration.sql
+++ prisma/migrations/20250212100505_init/migration.sql
@@ -13,6 +13,21 @@ CREATE TABLE "Products" (
 );
 
 -- CreateTable
+CREATE TABLE "Basket" (
+    "id" TEXT NOT NULL,
+    "itemCategory" TEXT NOT NULL,
+    "itemImg" TEXT NOT NULL,
+    "itemName" TEXT NOT NULL,
+    "itemDescription" TEXT,
+    "itemPrice" INTEGER NOT NULL,
+    "productId" TEXT NOT NULL,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Basket_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Address" (
     "id" TEXT NOT NULL,
     "userAddress" TEXT NOT NULL,
@@ -41,3 +56,6 @@ CREATE TABLE "Promocode" (
 
     CONSTRAINT "Promocode_pkey" PRIMARY KEY ("id")
 );
+
+-- AddForeignKey
+ALTER TABLE "Basket" ADD CONSTRAINT "Basket_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Products"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/components/MobileBasket.tsx
+++ src/components/MobileBasket.tsx
@@ -1,27 +1,25 @@
-"use client";
-//import { useGlobalContext } from "@/context/global-context";
-//import { useRouter } from "next/navigation";
-//import { FaCartShopping } from "react-icons/fa6";
+import prisma from "@/lib/db";
+import Link from "next/link";
+import { FaCartShopping } from "react-icons/fa6";
 
-export default function MobileBasket() {
-	//const { choosenPizza } = useGlobalContext();
-	//const router = useRouter();
+export default async function MobileBasket() {
+	const basketItems = await prisma.basket.findMany();
 
 	return (
-		<div className="hidden">
-			{/*{choosenPizza.length === 0 ? (
+		<div className="md:hidden">
+			{basketItems.length === 0 ? (
 				""
 			) : (
-				<div
-					onClick={() => router.push("/delivery")}
+				<Link
+					href="/delivery"
 					className="fixed bottom-6 right-4 z-10 bg-red-600 dark:bg-red-900 py-[.9rem] px-4 rounded-full"
 				>
 					<span className="flex items-center justify-center font-semibold tracking-wider text-white text-md">
 						<FaCartShopping className="mr-2" />
-						{choosenPizza && choosenPizza[0].itemPrice} zł
+						{basketItems && basketItems[0].itemPrice} zł
 					</span>
-				</div>
-			)}*/}
+				</Link>
+			)}
 		</div>
 	);
 }
